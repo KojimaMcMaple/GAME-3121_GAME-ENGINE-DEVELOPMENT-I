53bfc010fdd210d93b240b93eefee5c8a6cc0bc6
KojimaMcMaple <KyleHunter.KH@outlook.com>
2021-10-12 19:42:41 -0400

    remove sdk

--- ogre-sdk-v2.1/build/Components/RTShaderSystem/OgreRTShaderSystem_pch.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Hooman/GBC/GAME3121/Course Materials/GameEngineDevelopment/ogre-sdk-v2.1/Components/RTShaderSystem/src/OgreShaderPrecompiledHeaders.h"
--- ogre-sdk-v2.1/build/include/OgreConfigPaths.h
+++
@@ -1,8 +0,0 @@
-#ifndef OGRECONFIGPATHS_H_
-#define OGRECONFIGPATHS_H_
-
-#define OGRE_CONFIG_DIR "C:/Hooman/GBC/GAME3121/Course Materials/GameEngineDevelopment/ogre-sdk-v2.1/build/sdk/bin"
-#define OGRE_MEDIA_DIR "C:/Hooman/GBC/GAME3121/Course Materials/GameEngineDevelopment/ogre-sdk-v2.1/build/sdk/Media"
-#define OGRE_PLUGIN_DIR "C:/Hooman/GBC/GAME3121/Course Materials/GameEngineDevelopment/ogre-sdk-v2.1/build/sdk/bin" 
-
-#endif /* OGRECONFIGPATHS_H_ */
